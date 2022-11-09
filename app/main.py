b5e7f29e31974b3eae35db5926ee47495c0dd732
vmlucas <64113156+vmlucas@users.noreply.github.com>
2022-11-09 15:19:19 -0300

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -8,7 +8,6 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] =\
         'sqlite:///' + os.path.join(basedir, 'database.db')
